0867290c4cba420992e759159a7d9058b365920a
Priyanshu Singh <priyanshukumarsingh1502@gmail.com>
2026-08-24 11:42:34 +0530

    Add Employee class

---
+++ Employee Management System/Employee.java
@@ -0,0 +1,44 @@
+
+enum EmployeeStatus {
+    ACTIVE,
+    ON_LEAVE,
+    RESIGNED
+}
+
+abstract class Employee{
+    private final int employeeId;
+    private String name;
+    private String email;
+    private double baseSalary;
+    private EmployeeStatus status;         
+
+    public Employee(int employeeId, String name, String email, double  baseSalary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.email = email;
+        this.baseSalary = baseSalary;
+        status = EmployeeStatus.ACTIVE;
+    }
+
+    public int getId(){
+        return employeeId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    
+    public double getBaseSalary(){
+        return baseSalary;
+    }
+
+    public EmployeeStatus getEmployeeStatus(){
+        return status;
+    }
+
+    public abstract double calculateSalary();
+}
